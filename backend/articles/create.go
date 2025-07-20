ae792b82324e9ba5fd1d684ede16e16bcf1d17ad
milijan-mosic <milijan.mosic@gmail.com>
2025-07-20 23:46:18 +0200

    Creation of article is successful

--- backend/articles/create.go
+++ backend/articles/create.go
@@ -1,18 +1,53 @@
 package articles
 
 import (
+	"context"
+	"heartbit/database_interfaces"
+	"heartbit/utils"
+	"log"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
+	"github.com/google/uuid"
+	"github.com/jackc/pgx/v5"
 )
 
-func CreateArticle(c *gin.Context) {
-	status := http.StatusCreated
+func CreateArticleRoute(c *gin.Context) {
+	ctx := context.Background()
 
+	url := utils.GetDatabaseUrl()
+	conn, err := pgx.Connect(ctx, url)
+	if err != nil {
+		log.Fatalf("Couldn't connect to database: %s", err)
+	}
+	defer conn.Close(ctx)
+
+	queries := database_interfaces.New(conn)
+
+	article, err := queries.CreateArticle(ctx, database_interfaces.CreateArticleParams{
+		ID:        uuid.NewString(),
+		Title:     "Test title :)",
+		Author:    "Milijan Mosic",
+		Content:   "# Hello!\n",
+		Published: true,
+	})
+	if err != nil {
+		log.Fatalf("Error while creating article: %s", err)
+
+		status := http.StatusInternalServerError
+		response := HelloResponse{
+			Message: "Article creation failed!",
+			Status:  status,
+		}
+		c.JSON(status, response)
+	}
+
+	log.Printf("Created new article %v", article)
+
+	status := http.StatusCreated
 	response := HelloResponse{
 		Message: "Article created!",
 		Status:  status,
 	}
-
 	c.JSON(status, response)
 }
