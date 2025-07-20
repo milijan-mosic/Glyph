55e710086e607506000c458a538838bba9498bc9
milijan-mosic <milijan.mosic@gmail.com>
2025-07-20 23:46:30 +0200

    Listing of articles is successful

--- backend/articles/list.go
+++ backend/articles/list.go
@@ -1,18 +1,40 @@
 package articles
 
 import (
+	"context"
+	"heartbit/database_interfaces"
+	"heartbit/utils"
+	"log"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
+	"github.com/jackc/pgx/v5"
 )
 
-func ListArticle(c *gin.Context) {
-	status := http.StatusOK
+func ListArticleRoute(c *gin.Context) {
+	ctx := context.Background()
 
-	response := HelloResponse{
-		Message: "List of articles :)",
-		Status:  status,
+	url := utils.GetDatabaseUrl()
+	conn, err := pgx.Connect(ctx, url)
+	if err != nil {
+		log.Fatalf("Couldn't connect to database: %s", err)
+	}
+	defer conn.Close(ctx)
+
+	queries := database_interfaces.New(conn)
+
+	articles, err := queries.ListArticles(ctx)
+	if err != nil {
+		log.Fatalf("Error while listing articles: %s", err)
+
+		status := http.StatusInternalServerError
+		response := HelloResponse{
+			Message: "Articles listing failed!",
+			Status:  status,
+		}
+		c.JSON(status, response)
 	}
 
-	c.JSON(status, response)
+	status := http.StatusOK
+	c.JSON(status, articles)
 }
