3bb26ec6c0bd86f6963b448bc5e71b6ca16a257f
milijan-mosic <milijan.mosic@gmail.com>
2025-07-21 19:56:19 +0200

    Installed required frontend packages

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import tailwindcss from "@tailwindcss/vite";
+import path from "path";
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -8,6 +9,13 @@ export default defineConfig({
     port: 3000,
     host: true,
     allowedHosts: ["app.heartbit.local"],
+  },
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+      "@assets": path.resolve(__dirname, "./src/assets"),
+      "@components": path.resolve(__dirname, "./src/components"),
+    },
   },
   plugins: [react(), tailwindcss()],
 });
