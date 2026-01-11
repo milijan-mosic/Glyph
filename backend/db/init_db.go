16d3adeca1cab1c1f91b6c934ba2cf21efd4b95a
Milijan Mosic <milijan.mosic@gmail.com>
2026-01-11 20:08:42 +0100

    Fixed the database init. process

--- backend/db/init_db.go
+++ backend/db/init_db.go
@@ -10,17 +10,29 @@ import (
 
 var schema = `
 	CREATE TABLE articles (
-	  id   TEXT PRIMARY KEY,
-	  --
-	  title TEXT NOT NULL,
-	  description  TEXT,
-	  content TEXT NOT NULL,
-	  --
-	  author TEXT NOT NULL,
-	  published BOOLEAN NOT NULL,
-	  --
-	  created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-	  modified_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+		id   TEXT PRIMARY KEY,
+		--
+		title TEXT NOT NULL,
+		description TEXT NOT NULL,
+		content TEXT NOT NULL,
+		--
+		author TEXT NOT NULL,
+		published BOOLEAN NOT NULL,
+		--
+		created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+		modified_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+	);
+
+	CREATE TABLE comments (
+		id TEXT PRIMARY KEY,
+		--
+		article_id TEXT NOT NULL REFERENCES articles(id) ON DELETE CASCADE,
+		author_name TEXT NOT NULL,
+		content TEXT NOT NULL,
+		approved BOOLEAN NOT NULL DEFAULT FALSE,
+		--
+		created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+		modified_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 	);
 `
 
