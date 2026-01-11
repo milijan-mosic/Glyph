1ad12527439641aa5f538c35da470a29cb00bba9
Milijan Mosic <milijan.mosic@gmail.com>
2026-01-11 19:45:26 +0100

    Register handler for comments on main API

--- backend/main.go
+++ backend/main.go
@@ -1,7 +1,9 @@
 package main
 
 import (
-	"glyph/articles"
+	articles "glyph/blog/article"
+	comments "glyph/blog/comment"
+	"glyph/db"
 	"log"
 
 	"github.com/gin-gonic/gin"
@@ -14,16 +16,25 @@ var (
 
 func main() {
 	router := gin.Default()
+	
+	db.InitializeDatabase()
 
+	article := router.Group(urlPrefix + "/article")
 	{
-		articles.InitializeDatabase()
-		article := router.Group(urlPrefix + "/article")
-
 		article.GET("/list", articles.ListArticleRoute)
 		article.GET("/get/:articleId", articles.GetArticleRoute)
 		article.POST("/create", articles.CreateArticleRoute)
 		article.PUT("/update", articles.UpdateArticleRoute)
 		article.DELETE("/delete/:articleId", articles.DeleteArticleRoute)
+	}
+
+	comment := router.Group(urlPrefix + "/comment")
+	{
+		comment.POST("/comments", comments.CreateComment)
+		comment.GET("/posts/:postId/comments", comments.GetApprovedComments)
+		comment.GET("/admin/comments/pending", comments.GetPendingComments)
+		comment.PUT("/admin/comments/:id/approve", comments.ApproveComment)
+		comment.DELETE("/admin/comments/:id", comments.DeleteComment)
 	}
 
 	log.Printf("Server starting on: http://localhost%s\n", address)
